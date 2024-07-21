a58df589e07c31280232a7abfb4fa9274d7fcba5
sofyan <sofyan.salim@koltiva.com>
2024-07-21 08:28:29 +0700

    init

---
+++ streamlit_app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+APP_TITLE = "Fraud and Identity Theft Report"
+APP_SUB_TITLE = "Source: Federal Trade Comission"
+
+
+def main():
+    st.set_page_config(APP_TITLE)
+    st.title(APP_TITLE)
+
+
+if __name__ == "__main__":
+    main()
